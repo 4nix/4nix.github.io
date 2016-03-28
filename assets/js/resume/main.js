353a555f3eab65f2d71fe4d781cbd2b6cc564b4e
Wu Xutao <wublue1234@gmail.com>
2016-03-29 00:09:50 +0800

    resume v0.2

--- assets/js/resume/main.js
+++ assets/js/resume/main.js
@@ -7,7 +7,7 @@ require.config({
 });
 
 require(["canvas"], function(canvas) {
-  canvas.draw("main-ca", {});
+  canvas.draw("main-ca", {'后端': 0.8, '前端': 0.8, '服务器': 0.6, '数据库': 0.5, '其他': 0.4});
 });
 
 require(["aside"], function(aside) {
