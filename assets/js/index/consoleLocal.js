4368810230226cddf4582dc216adc76c1e421a8f
Wu Xutao <wublue1234@gmail.com>
2016-04-04 22:58:22 +0800

    change console

--- assets/js/index/consoleLocal.js
+++ assets/js/index/consoleLocal.js
@@ -6,16 +6,30 @@ define(['jquery'], function(jquery) {
 				color: #fff; \
 				line-height: 150px; \
 				background: \
-		        radial-gradient(10px at 25% 40%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(10px at 28% 55%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(10px at 22% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(60px at 25% 50%, transparent 0%, transparent 19%, #000 20%,#000 21%, transparent 22%), \
 		        radial-gradient(10px at 25% 50%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(60px at 25% 50%, #f00 0%, #f00 19%, #000 20%,#000 21%, #f00 22%, #f00 42%,#000 42%, #000 45%, transparent 46%), \
-		        radial-gradient(10px at 75% 40%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(10px at 78% 55%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(10px at 72% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(10px at 25% 40%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(18px at 27% 40%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 26% 39%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(10px at 27.5% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(20px at 26% 58%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 27% 58%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(10px at 22.5% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(18px at 22% 50%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 22% 53%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(60px at 25% 50%, #f00 0%, #f00 42%,#000 42%, #000 45%, transparent 46%), \
+		        radial-gradient(60px at 75% 50%, transparent 0%, transparent 19%, #000 20%,#000 21%, transparent 22%), \
 		        radial-gradient(10px at 75% 50%, #000 0%, #000 45%, transparent 46%), \
-		        radial-gradient(60px at 75% 50%, #f00 0%, #f00 19%, #000 20%,#000 21%, #f00 22%, #f00 42%,#000 42%, #000 45%, transparent 46%); \
+		        radial-gradient(10px at 75% 40%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(18px at 77% 40%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 76% 39%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(10px at 77.5% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(20px at 76% 58%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 77% 58%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(10px at 72.5% 55%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(18px at 72% 50%, #f00 0%, #f00 45%, transparent 46%), \
+		        radial-gradient(15px at 72% 53%, #000 0%, #000 45%, transparent 46%), \
+		        radial-gradient(60px at 75% 50%, #f00 0%, #f00 42%,#000 42%, #000 45%, transparent 46%); \
 		        background-color: #000;"
 				);
 
